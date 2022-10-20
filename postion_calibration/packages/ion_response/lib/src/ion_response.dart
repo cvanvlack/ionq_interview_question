f90697ec32198be99c238c3858e262a56a22b117
Cole Van Vlack <cvanvlack@gmail.com>
2022-10-20 12:51:38 -0400

    Added getter for num measurments

--- postion_calibration/packages/ion_response/lib/src/ion_response.dart
+++ postion_calibration/packages/ion_response/lib/src/ion_response.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:ion_response/ion_response.dart';
 
 /// {@template ion_response}
-/// This is the class the provides the interface for concrete implementations 
+/// This is the class the provides the interface for concrete implementations
 /// which are responsible for measuring the response from the ion.
 /// {@endtemplate}
 abstract class IonResponse {
@@ -15,6 +15,12 @@ abstract class IonResponse {
 
   final int _numMeasurements;
 
+  /// {@template num_measurements}
+  ///The number of measurements commanded. Required to calculate the percentage.
+  ///you get a different answer something is seriously wrong.
+  /// {@endtemplate}
+  int get numMeasurements => _numMeasurements;
+
   /// {@template min_num_photons}
   ///The minimum possible number of photons that can be measured is zero. If
   ///you get a different answer something is seriously wrong.
@@ -22,7 +28,7 @@ abstract class IonResponse {
   int get minNumPhotons => _minNumPhotons;
 
   /// {@template max_position}
-  /// The maximum possible number of photons that an be measured. If you get a 
+  /// The maximum possible number of photons that an be measured. If you get a
   /// different answer something is seriously wrong.
   /// {@endtemplate}
   int get maxNumPhotons => _numMeasurements;
